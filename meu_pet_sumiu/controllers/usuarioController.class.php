0692dd7322bdc1780ff6b39ae22298009e287a70
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2025-09-10 19:36:27 -0300

    aula-10-09

--- meu_pet_sumiu/controllers/usuarioController.class.php
+++ meu_pet_sumiu/controllers/usuarioController.class.php
@@ -1,14 +1,58 @@
 <?php
-    require_once "Models/usuarios.class.php"
+    require_once "models/conexao.class.php";
+    require_once "models/usuarioDAO.class.php";
+    require_once "models/usuarios.class.php";
     class UsuarioController
     {
         public function login() {
-            //require views formulário
+            
+            $msg = array("", "");
+            $erro = false;
             if($_POST)
             {
                 //verificar os dados
+                if(empty($_POST["email"]))
+                {
+                    $msg[0] = "Preencha o email!";
+                    $erro = true;
+                }
+                if(empty($_POST["senha"]))
+                {
+                    $msg[1] = "Preencha a senha!";
+                    $erro = true;
+                }
                 //verificar no banco de dados se existe esse usuário
+                if(!$erro)
+                {
+                    $usuario = new Usuarios(email:$_POST["email"]);
+                    $usuarioDAO = new usuarioDAO();
+                    $retorno = $usuarioDAO->login($usuario);
+                    // verificar se a senha corresponde
+                    if(is_array($retorno))
+                    {
+                        if(count($retorno) > 0)
+                        {
+                            //verificar se a senha corresponde
+                            if(password_verify($_POST['senha'],
+                            $retorno[0]->senha))
+                            {
+                                //logar
+                                $msg[2] = "Login com sucesso!!";
+                            }
+                            else
+                            {
+                                $msg[2] = "Verifique os dados digitados";
+                            }
+                        }
+                        else
+                        {
+                            $msg[2] = "Verifique os dados digitados";
+                        }
+                    }
+                }
             }
+            //require views formulário
+            require_once "views/login.php";
         }
 
         public function inserir() {
@@ -19,35 +63,47 @@
             {
                 if(empty($_POST["nome"]))
                 {
-                    $msg[0] = "Preencha o campo nome!"
+                    $msg[0] = "Preencha o campo nome!";
                     $erro = true;
                 }
 
                 if(empty($_POST["email"]))
                 {
-                    $msg[1] = "Preencha o campo email!"
+                    $msg[1] = "Preencha o campo email!";
                     $erro = true;
                 } else {
                     //verificar se não tem um usuário com esse email cadastrado
                     $usuario = new Usuarios(email: $_POST["email"]);
+                    $usuarioDAO = new usuarioDAO();
+                    $retorno = $usuarioDAO->verificar_email($usuario);
+                    if(is_array($retorno))
+                    {
+                        if(count($retorno) > 0)
+                        {
+                            $msg[1] = "Email já está cadastrado!";
+                            $erro = true;
+                        }
+                    }
                 }
 
                 if(empty($_POST["senha"]))
                 {
-                    $msg[2] = "Preencha o campo senha!"
+                    $msg[2] = "Preencha o campo senha!";
                     $erro = true;
                 }
 
                 if(empty($_POST["celular"]))
                 {
-                    $msg[3] = "Preencha o campo celular!"
+                    $msg[3] = "Preencha o campo celular!";
                     $erro = true;
                 }
 
                 if(!$erro)
                 {
-                    $usuario = new Usuarios(0, $_POST["nome"], $_POST["email"], $_POST["senha"], $_POST["celular"]);
+                    $usuario = new Usuarios(0, $_POST["nome"], $_POST["email"], password_hash($_POST["senha"], PASSWORD_DEFAULT), $_POST["celular"]);
                     //cadastrar no bd
+                    $usuarioDAO = new usuarioDAO();
+                    $retorno = $usuarioDAO->inserir($usuario);
                 }
             }
             require_once "views/form_usuario.php";
---
+++ meu_pet_sumiu/models/usuarioDAO.class.php
@@ -0,0 +1,67 @@
+<?php
+    class usuarioDAO extends Conexao
+    {
+        public function __construct()
+        {
+            parent:: __construct();
+        }
+
+        public function inserir($usuario) {
+            $sql = "INSERT INTO usuarios (nome, email, senha, celular) VALUES (?, ?, ?, ?)";
+
+            try
+            {
+                $stm = $this->db->prepare($sql);
+
+                $stm->bindValue(1, $usuario->getNome());
+                $stm->bindValue(2, $usuario->getEmail());
+                $stm->bindValue(3, $usuario->getSenha());
+                $stm->bindValue(4, $usuario->getCelular());
+                $stm->execute();
+                $this->db = null;
+                return "Usuário inserido com sucesso!";
+            }
+            catch(PDOException $e)
+            {
+                $this->db = null;
+                return "Problema com o cadastro do cliente";
+            }
+        }
+
+        public function login($usuario) {
+            $sql = "SELECT id, email, senha FROM usuarios WHERE email = ?";
+            try
+            {
+                $stm = $this->db->prepare($sql);
+                $stm->bindValue(1, $usuario->getEmail());
+                $stm->execute();
+                $this->db = null;
+
+                return $stm->fetchAll(PDO::FETCH_OBJ);
+            }
+            catch(PDOException $e)
+            {
+                $this->db = null;
+                return "Problema ao validar o email";
+            }
+        }
+
+        public function verificar_email($usuario) {
+            $sql = "SELECT email FROM usuarios WHERE email = ?";
+            try
+            {
+                $stm = $this->db->prepare($sql);
+                $stm->bindValue(1, $usuario->getEmail());
+                $stm->execute();
+                $this->db = null;
+
+                return $stm->fetchAll(PDO::FETCH_OBJ);
+            }
+            catch(PDOException $e)
+            {
+                $this->db = null;
+                return "Problema ao validar o email";
+            }
+        }
+    } //fim da classe
+?>
