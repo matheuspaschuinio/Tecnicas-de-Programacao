46a42cf6a7896444adc7cd26601736f6ed5aeaf2
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2025-09-17 17:06:17 -0300

    17-09

---
+++ meu_pet_sumiu/meu_pet_sumiu/models/conexao.class.php
@@ -0,0 +1,24 @@
+<?php
+    abstract class Conexao
+    {
+        public function __construct(protected $db = null) 
+        {
+            $parametros = "mysql:host=localhost;dbname=meupetsumiu;charset=utf8mb4";
+
+            try
+            {
+                $this->db = new PDO($parametros, "root", "");
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+                echo $e->getCode();
+                echo "Problema na conexão";
+                die();
+            }
+            
+        }
+
+        
+    }
+?>
